4e34adf9a812612316371db238ad71c0cb849b33
scdhuism <scdhuism@avans.nl>
2022-05-26 15:41:01 +0200

    Public service listens to all CUD commands

--- Harbor_ShipManagementService/controllers/shipCommand.controller.js
+++ Harbor_ShipManagementService/controllers/shipCommand.controller.js
@@ -79,6 +79,14 @@ const ShipUpdate = async (req, res, next) => {
         object: { _id: req.params.id, ...req.body },
       })
     );
+
+    await MQService.sendMessage(
+      'public',
+      JSON.stringify({
+        eventType: 'updateShip',
+        object: { _id: req.params.id, ...req.body },
+      })
+    );
     
     return res
       .status(200)
@@ -103,6 +111,11 @@ const ShipDelete = (req, res, next) => {
 
     await MQService.sendMessage(
       'company',
+      JSON.stringify({ eventType: 'deleteShip', object: fueltank })
+    );
+
+    await MQService.sendMessage(
+      'public',
       JSON.stringify({ eventType: 'deleteShip', object: fueltank })
     );
 
